ce62b170213e7b0473d1aecc04ecf80b9d51060f
Simon Puckey <simonpuckey@hotmail.com>
2023-10-20 23:16:22 +0100

    Upgraded payload and react-query to new major versions - some breaking changes. Some tidy up remains

--- payload-in-next/payload/payload.config.ts
+++ payload-in-next/payload/payload.config.ts
@@ -1,25 +1,22 @@
 import path from "path";
 import { buildConfig } from "payload/config";
+import { mongooseAdapter } from "@payloadcms/db-mongodb";
+import { slateEditor } from "@payloadcms/richtext-slate";
 import Categories from "./collections/Categories";
 import Posts from "./collections/Posts";
 import Tags from "./collections/Tags";
+import Users from "./collections/Users";
 
-// TODO: got users as default but override with own Users as per standalone app?
-// TODO: not adding express or gql right now as want to try local api
 export default buildConfig({
-  // admin: {
-  //   user: Users.slug,
-  // },
-  // collections: [Categories, Posts, Tags, Media],
-  // excluding media as could mean 'sharp' is included
-  // which is a problem because then cannot find it remotely
-  // even though vercel claims to install it by default...
-  // https://github.com/payloadcms/next-payload/issues/72#issuecomment-1743760245
-  collections: [Categories, Posts, Tags],
+  collections: [Categories, Posts, Tags, Users],
   globals: [
     // Your globals here
   ],
   typescript: {
     outputFile: path.resolve(__dirname, "../payload-types.ts"),
   },
+  db: mongooseAdapter({
+    url: process.env.MONGODB_URI as string,
+  }),
+  editor: slateEditor({}),
 });
