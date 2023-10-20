27b0d6fc0151c33eb87061bff34a29c31169246c
Simon Puckey <simonpuckey@hotmail.com>
2023-10-20 22:37:38 +0100

    Removed Media collection in attempt to stop Payload looking for 'sharp' on Vercel CI

--- payload-in-next/payload/payload.config.ts
+++ payload-in-next/payload/payload.config.ts
@@ -1,7 +1,6 @@
 import path from "path";
 import { buildConfig } from "payload/config";
 import Categories from "./collections/Categories";
-import Media from "./collections/Media";
 import Posts from "./collections/Posts";
 import Tags from "./collections/Tags";
 
